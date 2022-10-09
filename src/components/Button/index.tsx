e00fcfc20cabbdf20026e10418752ec24de374ce
shkirya.ns <shkirya.ns@oilpc.ru>
2022-10-09 18:32:12 +0600

    added tooltip for Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import Icons from "components/Icons";
+import Tooltip, {TooltipTypes} from "../Tooltip";
 
 type ButtonProps = {
     children?: React.ReactNode
     disabled?: boolean
+    tooltip?: string
 } & React.ButtonHTMLAttributes<any>
 
 const Button = (props: ButtonProps) => {
-    return <button {...props} className={`btn ${props.className}`} disabled={props.disabled}>{props.children}</button>
+    return <>
+        <button
+            {...props} className={`btn ${props.className}`} disabled={props.disabled}
+            data-tip
+            data-for={`Button${props.tooltip}`}
+        >
+            {props.children}
+        </button>
+        {props.tooltip && <Tooltip
+            type={TooltipTypes.primary}
+            body={<span>{props.tooltip}</span>}
+            id={`Button${props.tooltip}`}
+        />}
+    </>
 }
 
 export const ButtonRemove = (props: ButtonProps) => {
